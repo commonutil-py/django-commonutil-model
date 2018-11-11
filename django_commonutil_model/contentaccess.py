3a0abfd88d8e3ecd2561cb445205cff78ccc1f78
Cheyin L <cheyinl@gmail.com>
2018-11-11 18:58:04 +0800

    refactor: pack into package

--- django_commonutil_model/contentaccess.py
+++ django_commonutil_model/contentaccess.py
@@ -1,51 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-Utilities for Django Model and Model Field
+Access content of Django model instances
 """
 
 from __future__ import unicode_literals
 
 from django.db.models.fields.related import RelatedField, ForeignKey
-
-
-def _make_default_kwd_method(orig_method, k, v):
-	""" (internal)
-	"""
-
-	def f(self, *args, **kwds):
-		kwds.setdefault(k, v)
-		return orig_method(self, *args, **kwds)
-
-	return f
-
-
-def _make_default_kwd_modelfield_deconstruct(orig_deconstruct, k, v):
-	""" (internal)
-	"""
-
-	def f(self, *args, **kwds):
-		name, path, dargs, dkwds, = orig_deconstruct(self, *args, **kwds)
-		if not (dkwds.get(k) is v):
-			del dkwds[k]
-		return name, path, dargs, dkwds
-
-	return f
-
-
-def attach_default_help_text_for_modelfield(help_text):
-	"""
-	Decorator to attach default help text for model field
-
-	Args:
-		help_text - Help text
-	"""
-
-	def f(cls):
-		cls.__init__ = _make_default_kwd_method(cls.__init__, "help_text", help_text)
-		cls.deconstruct = _make_default_kwd_modelfield_deconstruct(cls.deconstruct, "help_text", help_text)
-		return cls
-
-	return f
 
 
 def fetch_model_field_contents(model_cls, instance, include_none=False):
